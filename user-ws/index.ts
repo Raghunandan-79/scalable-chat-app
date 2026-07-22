b1073d76d71f43882c8bb002f3cb1dd87045ceba
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-22 19:04:53 +0530

    Added logic to connect with relayer socket

--- user-ws/index.ts
+++ user-ws/index.ts
@@ -1,19 +1,43 @@
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocketServer, WebSocket as WebSocketWsType } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 interface Room {
-  sockets: WebSocket[];
+  sockets: WebSocketWsType[];
 }
 
 const rooms: Record<string, Room> = {};
 
+const RELAYER_URL = "ws://localhost:3001";
+const relayerSocket = new WebSocket(RELAYER_URL);
+
+relayerSocket.onmessage = ({ data }) => {
+  const parsedData = JSON.parse(data);
+
+  if (parsedData.type == "chat") {
+    const room = parsedData.room;
+    rooms[room]?.sockets.map((socket) => socket.send(data));
+  }
+};
+
 wss.on("connection", (ws) => {
   ws.on("error", console.error);
 
-  ws.on("message", (data) => {
-    console.log(`received: %s`, data);
-  });
+  ws.on("message", (data: string) => {
+    const parsedData = JSON.parse(data);
 
-  ws.send("something");
+    if (parsedData.type == "join-room") {
+      const room = parsedData.room;
+      if (!rooms[room]) {
+        rooms[room] = {
+          sockets: [],
+        };
+      }
+      rooms[room].sockets.push(ws);
+    }
+
+    if (parsedData.type == "chat") {
+      relayerSocket.send(data);
+    }
+  });
 });
